e51481a383b9e282c28e54fbdbf3604d41b80329
David Anderson <danderson@tailscale.com>
2023-07-20 15:05:23 -0700

    client/setec: block out a client API
    No implementation yet, just getting a feeling for the shape of the API.
    
    Updates tailscale/corp#13375
    
    Signed-off-by: David Anderson <danderson@tailscale.com>

---
+++ client/setec/client.go
@@ -0,0 +1,122 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package setec is a client library to access and manage secrets
+// stored remotely in a secret management service.
+package setec
+
+import (
+	"context"
+	"errors"
+)
+
+// Store is a store that provides named secrets.
+type Store struct {
+}
+
+// Cache is an interface that lets a Store persist one piece of data
+// locally. The persistence need not be perfect (i.e. it's okay to
+// lose previously written data).
+type Cache interface {
+	// Write persists the given bytes for future retrieval.
+	Write([]byte) error
+	// Read returns previously persisted bytes, if any are available.
+	Read() ([]byte, error)
+}
+
+// StoreConfig is the configuration for Store.
+type StoreConfig struct {
+	// Client is the client to use to fetch secrets.
+	Client Client
+	// Cache, if non-nil, is a cache that can save secrets locally.
+	//
+	// Depending on the implementation, local caching may degrade
+	// security slightly by making secrets easier to get at, but in
+	// return allows the Store to initialize and run during outages of
+	// the secrets management service.
+	//
+	// If no cache is provided, the Store uses an ephemeral in-memory
+	// cache that lasts the lifetime of the process only.
+	Cache Cache
+	// Secrets are the names of the secrets this Store should
+	// retrieve. Only secrets named here can be read out of the store.
+	Secrets []string
+}
+
+// NewStore creates a secret store with the given configuration.
+// NewStore blocks until the secrets named in cfg.Secrets are
+// available for retrieval through Secret().
+func NewStore(ctx context.Context, cfg *StoreConfig) (*Store, error) {
+	return nil, errors.New("not implemented")
+}
+
+// Secret returns the value of the named secret.
+func (s *Store) Secret(name string) []byte {
+	return nil
+}
+
+// Client is a raw client to the secret management server.
+// If you're just consuming secrets, you probably want to use a Store
+// instead.
+type Client struct {
+	// Server is the URL of the secrets server to talk to.
+	Server string
+}
+
+// List fetches a list of secrets and associated metadata (but not the
+// secret values themselves).
+func (c *Client) List(ctx context.Context) ([]*SecretInfo, error) {
+	return nil, errors.New("not implemented")
+}
+
+// Get fetches a secret value by name.
+func (c *Client) Get(ctx context.Context, name string) ([]*SecretValue, error) {
+	return nil, errors.New("not implemented")
+}
+
+// Get fetches a secret value by name and version.
+func (c *Client) GetVersion(ctx context.Context, name string, version uint32) ([]*SecretValue, error) {
+	return nil, errors.New("not implemented")
+}
+
+// Info fetches metadata for a given secret name.
+func (c *Client) Info(ctx context.Context, name string) (*SecretInfo, error) {
+	return nil, errors.New("not implemented")
+}
+
+// Put creates a secret called name, with the given value. If a secret
+// called name already exist, the value is saved as a new inactive
+// version.
+func (c *Client) Put(ctx context.Context, name string, value []byte) (version uint32, err error) {
+	return 0, errors.New("not implemented")
+}
+
+// SetActiveVersion changes the active version of the secret called
+// name to version.
+func (c *Client) SetActiveVersion(ctx context.Context, name string, version uint32) error {
+	return errors.New("not implemented")
+}
+
+// Secret is a secret value and its associated version.
+//
+// Versions are a positive integer assigned by the secrets server when
+// secrets are created or updated. Successive updates to a secret
+// return a higher version than before, so the versions can be used as
+// a chronological order of secret values.
+type SecretValue struct {
+	Value   []byte
+	Version uint32
+}
+
+// SecretInfo is information about a named secret.
+//
+// A secret has one or more versions (think: updating the API key used
+// to talk to a service). One of the versions is always marked
+// "active", and is the version served to clients that request a
+// secret without providing an explicit version. Inactive secret
+// versions can be deleted.
+type SecretInfo struct {
+	Name          string
+	Versions      []uint32
+	ActiveVersion uint32
+}
