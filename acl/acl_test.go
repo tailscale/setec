17a42366edc0ef15917097c0e4014ce6b0c925df
David Anderson <danderson@tailscale.com>
2023-07-21 09:51:12 -0700

    acl: an ACL parser and evaluator
    Updates tailscale/corp#13375
    
    Signed-off-by: David Anderson <danderson@tailscale.com>

---
+++ acl/acl_test.go
@@ -0,0 +1,134 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+
+package acl_test
+
+import (
+	"testing"
+
+	"github.com/tailscale/setec/acl"
+)
+
+func TestACL(t *testing.T) {
+	const src = `
+{
+  "groups": {
+    "admins": ["a@ts.net", "b@ts.net"],
+    "servers": ["tag:server", "tag:lab"],
+  },
+  "rules": [
+    {
+      "principal": ["tag:control"],
+      "action": ["get"],
+      "secret": ["control/foo", "control/bar"],
+    },
+    {
+      "principal": ["group:servers", "c@ts.net"],
+      "action": ["get"],
+      "secret": ["quux"],
+    },
+    {
+      "principal": ["group:admins"],
+      "action": ["get", "list", "put", "set-active", "delete"],
+      "secret": ["*"],
+    },
+    {
+      "principal": ["c@ts.net"],
+      "action": ["get", "list", "put", "set-active", "delete"],
+      "secret": ["dev/*", "prod/*/some-secret"],
+    },
+  ],
+}`
+	pol, err := acl.Compile([]byte(src))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type testCase struct {
+		from   []string
+		secret string
+		action acl.Action
+		want   bool
+	}
+	allow := func(action, secret string, from ...string) testCase {
+		return testCase{from, secret, acl.Action(action), true}
+	}
+	deny := func(action, secret string, from ...string) testCase {
+		return testCase{from, secret, acl.Action(action), false}
+	}
+	tests := []testCase{
+		allow("get", "control/foo", "tag:control"),
+		allow("get", "control/foo", "tag:other", "tag:control"),
+		allow("get", "control/bar", "tag:control"),
+		deny("get", "control/quux", "tag:control"),
+		deny("get", "control/foo", "tag:other"),
+		deny("list", "control/foo", "tag:control"),
+		deny("put", "control/foo", "tag:control"),
+		deny("set-active", "control/foo", "tag:control"),
+		deny("delete", "control/foo", "tag:control"),
+		deny("put", "control/other", "tag:control"),
+
+		allow("get", "quux", "tag:server"),
+		allow("get", "quux", "tag:lab"),
+		allow("get", "quux", "tag:other", "tag:lab"),
+		allow("get", "quux", "tag:other", "tag:server", "tag:lab"),
+		allow("get", "quux", "c@ts.net"),
+		deny("put", "quux", "c@ts.net"),
+		deny("put", "quux", "tag:server"),
+		deny("put", "quux", "tag:other"),
+
+		allow("get", "control/foo", "a@ts.net"),
+		allow("put", "control/foo", "b@ts.net"),
+		allow("list", "quux", "b@ts.net"),
+		allow("set-active", "quux", "b@ts.net"),
+		allow("delete", "quux", "b@ts.net"),
+
+		allow("get", "dev/foo", "c@ts.net"),
+		allow("put", "dev/foo", "c@ts.net"),
+		allow("list", "dev/bar", "c@ts.net"),
+		allow("set-active", "dev/bar", "c@ts.net"),
+		allow("delete", "dev/bar", "c@ts.net"),
+		allow("get", "prod/foo/some-secret", "c@ts.net"),
+		allow("put", "prod/foo/some-secret", "c@ts.net"),
+		allow("list", "prod/quux/some-secret", "c@ts.net"),
+		allow("set-active", "prod/other/some-secret", "c@ts.net"),
+		allow("delete", "prod/wat/some-secret", "c@ts.net"),
+		deny("get", "prod/other/suffix", "c@ts.net"),
+		deny("get", "prod/some-secret", "c@ts.net"),
+	}
+
+	for _, test := range tests {
+		if got := pol.Allow(test.from, test.secret, test.action); got != test.want {
+			t.Errorf("Allow(%v, %q, %q) = %v, want %v", test.from, test.secret, test.action, got, test.want)
+		}
+	}
+}
+
+func TestInvalidPolicy(t *testing.T) {
+	reject := map[string]string{
+		"gibberish":        `gthgoht34h89`,
+		"unknown-field":    `{"blork": 42}`,
+		"wrong-group-type": `{"groups": "I'm a little teapot"}`,
+		"wrong-rules-type": `{"rules": "short and stout"}`,
+		"missing-group": `{
+  "rules": [{
+    "action": ["get"],
+    "secret": ["*"],
+    "principal": ["group:missing"],
+  }]
+}`,
+		"unknown-action": `{
+  "rules": [{
+    "action": ["oscillate-the-overthruster"],
+    "secret": ["*"],
+    "principal": ["a@ts.net"],
+  }]
+}`,
+	}
+
+	for n, r := range reject {
+		if _, err := acl.Compile([]byte(r)); err == nil {
+			t.Errorf("parse %q succeeded, want error", n)
+		}
+	}
+}
