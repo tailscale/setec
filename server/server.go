e94024897cc42847ebd577d2842f88eafd86539f
David Anderson <danderson@tailscale.com>
2023-07-28 15:27:51 -0700

    server: an HTTP server for the secrets database
    Updates tailscale/corp#13375
    
    Signed-off-by: David Anderson <danderson@tailscale.com>

---
+++ server/server.go
@@ -0,0 +1,155 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package server implements the setec secrets server.
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/tailscale/setec/db"
+	"github.com/tailscale/setec/types/api"
+	"github.com/tink-crypto/tink-go/v2/tink"
+	"tailscale.com/client/tailscale/apitype"
+)
+
+// Config is the configuration for a Server.
+type Config struct {
+	// DBPath is the path to the secrets database.
+	DBPath string
+	// Key is the AEAD used to encrypt/decrypt the database.
+	Key tink.AEAD
+	// WhoIs is a function that reports an identity for a client IP
+	// address. Outside of tests, it will be the WhoIs of a Tailscale
+	// LocalClient.
+	WhoIs func(ctx context.Context, remoteAddr string) (*apitype.WhoIsResponse, error)
+	// Mux is the http.ServeMux on which the server registers its HTTP
+	// handlers.
+	Mux *http.ServeMux
+}
+
+// Server is a secrets HTTP server.
+type Server struct {
+	db    *db.DB
+	whois func(context.Context, string) (*apitype.WhoIsResponse, error)
+}
+
+// New creates a secret server and makes it ready to serve.
+func New(cfg Config) (*Server, error) {
+	db, err := db.Open(cfg.DBPath, cfg.Key)
+	if err != nil {
+		return nil, fmt.Errorf("opening DB: %w", err)
+	}
+
+	ret := &Server{
+		db:    db,
+		whois: cfg.WhoIs,
+	}
+	cfg.Mux.HandleFunc("/api/list", ret.list)
+	cfg.Mux.HandleFunc("/api/get", ret.get)
+	cfg.Mux.HandleFunc("/api/info", ret.info)
+	cfg.Mux.HandleFunc("/api/put", ret.put)
+	cfg.Mux.HandleFunc("/api/set-active", ret.setActive)
+
+	return ret, nil
+}
+
+func (s *Server) list(w http.ResponseWriter, r *http.Request) {
+	serveJSON(s, w, r, func(req api.ListRequest, from []string) ([]*api.SecretInfo, error) {
+		return s.db.List(from)
+	})
+}
+
+func (s *Server) get(w http.ResponseWriter, r *http.Request) {
+	serveJSON(s, w, r, func(req api.GetRequest, from []string) (*api.SecretValue, error) {
+		if req.Version != 0 {
+			return s.db.GetVersion(req.Name, req.Version, from)
+		}
+		return s.db.Get(req.Name, from)
+	})
+}
+
+func (s *Server) info(w http.ResponseWriter, r *http.Request) {
+	type request struct {
+		Name string
+	}
+	serveJSON(s, w, r, func(req api.InfoRequest, from []string) (*api.SecretInfo, error) {
+		return s.db.Info(req.Name, from)
+	})
+}
+
+func (s *Server) put(w http.ResponseWriter, r *http.Request) {
+	serveJSON(s, w, r, func(req api.PutRequest, from []string) (api.SecretVersion, error) {
+		return s.db.Put(req.Name, req.Value, from)
+	})
+}
+
+func (s *Server) setActive(w http.ResponseWriter, r *http.Request) {
+	serveJSON(s, w, r, func(req api.SetActiveRequest, from []string) (struct{}, error) {
+		if err := s.db.SetActiveVersion(req.Name, req.Version, from); err != nil {
+			return struct{}{}, err
+		}
+		return struct{}{}, nil
+	})
+}
+
+// getIdentity extracts a Tailscale identity from an HTTP request.
+func (s *Server) getIdentity(r *http.Request) ([]string, error) {
+	who, err := s.whois(r.Context(), r.RemoteAddr)
+	if err != nil {
+		return nil, fmt.Errorf("calling WhoIs: %w", err)
+	}
+
+	if who.UserProfile != nil && who.UserProfile.LoginName != "" {
+		return []string{who.UserProfile.LoginName}, nil
+	} else if who.Node != nil && len(who.Node.Tags) > 0 {
+		return who.Node.Tags, nil
+	}
+	return nil, errors.New("failed to find caller identity")
+}
+
+// serveJSON calls fn to handle a JSON API request. fn is invoked with
+// the request body decoded into r, and from set to the Tailscale
+// identity of the caller. The response returned from fn is serialized
+// as JSON back to the client.
+func serveJSON[REQ any, RESP any](s *Server, w http.ResponseWriter, r *http.Request, fn func(r REQ, from []string) (RESP, error)) {
+	if c := r.Header.Get("Content-Type"); c != "application/json" {
+		http.Error(w, "request body must be json", http.StatusBadRequest)
+		return
+	}
+
+	from, err := s.getIdentity(r)
+	if err != nil {
+		http.Error(w, "unable to identify caller", http.StatusInternalServerError)
+		return
+	}
+
+	var req REQ
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	resp, err := fn(req, from)
+	if errors.Is(err, db.ErrAccessDenied) {
+		http.Error(w, "access denied", http.StatusForbidden)
+		return
+	} else if err != nil {
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		return
+	}
+
+	bs, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, "failed to encode respnse", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(bs)
+}
--- types/api/api.go
+++ types/api/api.go
@@ -14,6 +14,11 @@ package api
 // be interpreted as a chronological order of the secret's values.
 type SecretVersion uint32
 
+// SecretVersionDefault is a version that means the client wants the
+// server to pick an appropriate secret version. Currently, the server
+// translates this to the version marked active.
+const SecretVersionDefault SecretVersion = 0
+
 // Secret is a secret value and its associated version.
 type SecretValue struct {
 	Value   []byte
@@ -30,4 +35,39 @@ type SecretInfo struct {
 	Name          string
 	Versions      []SecretVersion
 	ActiveVersion SecretVersion
+}
+
+// ListRequest is a request to list secrets.
+type ListRequest struct{}
+
+// GetRequest is a request to get a secret value.
+type GetRequest struct {
+	// Name is the name of the secret to fetch.
+	Name string
+	// Version is the version to fetch, or SecretVersionDefault to let
+	// the server pick a version.
+	Version SecretVersion
+}
+
+// InfoRequest is a request for secret metadata.
+type InfoRequest struct {
+	// Name is the name of the secret whose metadata to return.
+	Name string
+}
+
+// PutRequest is a request to write a secret value.
+type PutRequest struct {
+	// Name is the name of the secret to write.
+	Name string
+	// Value is the secret value.
+	Value []byte
+}
+
+// SetActiveRequest is a request to change the active version of a
+// secret.
+type SetActiveRequest struct {
+	// Name is the name of the secret to update.
+	Name string
+	// Version is the version to make active.
+	Version SecretVersion
 }
