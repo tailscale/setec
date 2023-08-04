308d2db5f539e2a318e900c2474cf300e4adc47b
David Anderson <danderson@tailscale.com>
2023-08-04 14:22:09 -0700

    db: generate audit entries on use, plumb through server
    Updates tailscale/corp#13375
    
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- db/db.go
+++ db/db.go
@@ -27,12 +27,14 @@ import (
 	"github.com/tailscale/setec/types/api"
 	"github.com/tink-crypto/tink-go/v2/tink"
 	"golang.org/x/exp/slices"
+	"tailscale.com/util/multierr"
 )
 
 // DB is an encrypted secrets database.
 type DB struct {
-	mu sync.Mutex
-	kv *kv
+	mu       sync.Mutex
+	kv       *kv
+	auditLog *audit.Writer
 }
 
 // We might store some of setec's configuration in the secrets
@@ -52,14 +54,19 @@ var (
 
 // Open loads the secrets database at path, decrypting it using key.
 // If no database exists at path, a new empty database is created.
-func Open(path string, key tink.AEAD) (*DB, error) {
+func Open(path string, key tink.AEAD, auditLog *audit.Writer) (*DB, error) {
+	if auditLog == nil {
+		return nil, errors.New("must provide an audit.Writer to db.Open")
+	}
+
 	kv, err := openOrCreateKV(path, key)
 	if err != nil {
 		return nil, err
 	}
 
 	ret := &DB{
-		kv: kv,
+		kv:       kv,
+		auditLog: auditLog,
 	}
 
 	return ret, nil
@@ -76,11 +83,51 @@ type Caller struct {
 	Permissions acl.Rules
 }
 
+// checkAndLog verifies that caller can perform action on secret, and
+// writes an appropriate audit log entry.
+// The caller must not perform the requested operation if an error is
+// returned.
+func (db *DB) checkAndLog(caller Caller, action acl.Action, secret string, secretVersion api.SecretVersion) error {
+	var errs []error
+	authorized := caller.Permissions.Allow(action, secret)
+	if !authorized {
+		errs = append(errs, ErrAccessDenied)
+	}
+	err := db.auditLog.WriteEntries(&audit.Entry{
+		Principal:     caller.Principal,
+		Action:        action,
+		Secret:        secret,
+		SecretVersion: secretVersion,
+		Authorized:    authorized,
+	})
+	if err != nil {
+		errs = append(errs, fmt.Errorf("writing audit log: %w", err))
+	}
+	return multierr.New(errs...)
+}
+
 // List returns secret metadata for all secrets on which at least one
 // member of 'from' has acl.ActionList permissions.
 func (db *DB) List(caller Caller) ([]*api.SecretInfo, error) {
 	db.mu.Lock()
 	defer db.mu.Unlock()
+
+	// List is unusual, because we don't check a permission
+	// upfront. Instead, we return the output of Info() for every
+	// secret the caller can access.
+	//
+	// To avoid spamming the audit log, we record a single audit entry
+	// to reflect that List took place, then do per-secret permission
+	// checks to construct the response without generating individual
+	// audit entries there.
+	err := db.auditLog.WriteEntries(&audit.Entry{
+		Principal:  caller.Principal,
+		Action:     acl.ActionInfo,
+		Authorized: true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("writing audit log: %w", err)
+	}
 
 	var ret []*api.SecretInfo
 	for _, name := range db.kv.list() {
@@ -99,8 +146,8 @@ func (db *DB) List(caller Caller) ([]*api.SecretInfo, error) {
 
 // Info returns metadata for the given secret.
 func (db *DB) Info(caller Caller, name string) (*api.SecretInfo, error) {
-	if !caller.Permissions.Allow(acl.ActionInfo, name) {
-		return nil, ErrAccessDenied
+	if err := db.checkAndLog(caller, acl.ActionInfo, name, 0); err != nil {
+		return nil, err
 	}
 
 	db.mu.Lock()
@@ -110,8 +157,8 @@ func (db *DB) Info(caller Caller, name string) (*api.SecretInfo, error) {
 
 // Get returns a secret's active value.
 func (db *DB) Get(caller Caller, name string) (*api.SecretValue, error) {
-	if !caller.Permissions.Allow(acl.ActionGet, name) {
-		return nil, ErrAccessDenied
+	if err := db.checkAndLog(caller, acl.ActionGet, name, 0); err != nil {
+		return nil, err
 	}
 
 	db.mu.Lock()
@@ -121,8 +168,8 @@ func (db *DB) Get(caller Caller, name string) (*api.SecretValue, error) {
 
 // GetVersion returns a secret's value at a specific version.
 func (db *DB) GetVersion(caller Caller, name string, version api.SecretVersion) (*api.SecretValue, error) {
-	if !caller.Permissions.Allow(acl.ActionGet, name) {
-		return nil, ErrAccessDenied
+	if err := db.checkAndLog(caller, acl.ActionGet, name, version); err != nil {
+		return nil, err
 	}
 
 	db.mu.Lock()
@@ -138,8 +185,8 @@ func (db *DB) Put(caller Caller, name string, value []byte) (api.SecretVersion, 
 	if name == "" {
 		return 0, errors.New("empty secret name")
 	}
-	if !caller.Permissions.Allow(acl.ActionPut, name) {
-		return 0, ErrAccessDenied
+	if err := db.checkAndLog(caller, acl.ActionPut, name, 0); err != nil {
+		return 0, err
 	}
 
 	db.mu.Lock()
@@ -163,8 +210,8 @@ func (db *DB) SetActiveVersion(caller Caller, name string, version api.SecretVer
 	if name == "" {
 		return errors.New("empty secret name")
 	}
-	if !caller.Permissions.Allow(acl.ActionSetActive, name) {
-		return ErrAccessDenied
+	if err := db.checkAndLog(caller, acl.ActionSetActive, name, version); err != nil {
+		return err
 	}
 
 	db.mu.Lock()
