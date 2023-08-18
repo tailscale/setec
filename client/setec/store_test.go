649098029a33fd128b78b8707cc9a868e64c4639
M. J. Fromberger <fromberger@tailscale.com>
2023-08-17 18:57:09 -0700

    client: add a Watcher type for secret values
    For clients that need to be notified when a new secret value is available, the
    Watcher encapsulates a Secret with a level-triggered notification channel.

--- client/setec/store_test.go
+++ client/setec/store_test.go
@@ -273,6 +273,64 @@ func TestSlowInit(t *testing.T) {
 	checkSecretValue(t, st, "boo", "go for the eyes")
 }
 
+func TestWatcher(t *testing.T) {
+	d := setectest.NewDB(t, nil)
+	d.MustPut(d.Superuser, "green", "eggs and ham") // active
+	v2 := d.MustPut(d.Superuser, "green", "grow the rushes oh")
+
+	ts := setectest.NewServer(t, d, nil)
+	hs := httptest.NewServer(ts.Mux)
+	defer hs.Close()
+
+	ctx := context.Background()
+	cli := setec.Client{Server: hs.URL, DoHTTP: hs.Client().Do}
+
+	pollTicker := newFakeTicker()
+	st, err := setec.NewStore(ctx, setec.StoreConfig{
+		Client:     cli,
+		Secrets:    []string{"green"},
+		PollTicker: pollTicker,
+	})
+	if err != nil {
+		t.Fatalf("NewStore: unexpected error: %v", err)
+	}
+	defer st.Close()
+
+	// Observe the initial value of the secret.
+	w := st.Watcher("green")
+	if got, want := string(w.Get()), "eggs and ham"; got != want {
+		t.Errorf("Initial value: got %q, want %q", got, want)
+	}
+
+	// The secret gets updated...
+	if err := cli.SetActiveVersion(ctx, "green", v2); err != nil {
+		t.Fatalf("SetActiveVersion to %v: unexpected error: %v", v2, err)
+	}
+
+	// The next poll occurs...
+	pollTicker.Poll()
+
+	// The watcher should get notified in a timely manner.
+	select {
+	case <-w.Ready():
+		t.Logf("✓ A new version of the secret is available")
+	case <-time.After(5 * time.Second):
+		t.Fatal("Timed out waiting for a watcher update")
+	}
+
+	if got, want := string(w.Get()), "grow the rushes oh"; got != want {
+		t.Errorf("Updated value: got %q, want %q", got, want)
+	}
+
+	// With no updates, the watchers should not appear ready.
+	select {
+	case <-w.Ready():
+		t.Error("Watcher is unexpectedly ready after no update")
+	case <-time.After(100 * time.Millisecond):
+		// OK
+	}
+}
+
 type badCache struct{}
 
 func (badCache) Write([]byte) error    { return errors.New("write failed") }
