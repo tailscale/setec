4c783388a55c16caa159a60fc69080f9e54bd0d7
jj hunt <j@me.net.nz>
2024-05-07 17:18:16 +0000

    Warn that you need to update version.

--- cmd/setec/setec.go
+++ cmd/setec/setec.go
@@ -441,6 +441,9 @@ func runPut(env *command.Env, name string) error {
 		return fmt.Errorf("failed to write secret: %w", err)
 	}
 	fmt.Printf("Secret saved as %q, version %d\n", name, ver)
+	if ver > 1 {
+		fmt.Printf("Be aware the default version of the secret is not automatically incremented. To make this version the default run setec activate %s %d\n", name, ver)
+	}
 	return nil
 }
 
