e5a2e49f058b86588e54502ff5b350c719d35eef
M. J. Fromberger <fromberger@tailscale.com>
2026-02-18 14:08:18 -0800

    all: remove the AUTHORS file and update comments (#160)
    See tailscale/tailscale#18503 for context

--- cmd/setec/setec.go
+++ cmd/setec/setec.go
@@ -1,4 +1,4 @@
-// Copyright (c) Tailscale Inc & AUTHORS
+// Copyright (c) Tailscale Inc & contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
 // Program setec is a secret management server that vends secrets over
